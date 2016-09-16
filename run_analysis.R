e2ea9210bc6912c32783b3c5ccd6f386712097fe
Uday Kumar Vaddina <udayvaddina@gmail.com>
2016-09-16 15:14:18 -0500

    Read tables from the dataset

--- run_analysis.R
+++ run_analysis.R
@@ -15,4 +15,22 @@ if (!file.exists('./data/UCI HAR Dataset.zip')){
 
 
 # unzip dataset into the folder data
-unzip(zipfile="./data/Dataset.zip",exdir="./data")
+unzip(zipfile="./data/UCI HAR Dataset.zip",exdir="./data")
+
+
+# Reading train tables
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+
+# Reading test tables
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+# Reading activity labels
+activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+
+#Reading feature vector
+features <- read.table("./data/UCI HAR Dataset/features.txt")
